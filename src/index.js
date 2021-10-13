ce0fe512a1e114ca74e782e34048635b8f676d9d
Roman <romankp@gmail.com>
2021-10-12 22:07:34 -0400

    tryin' web components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-// Test
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    * {
+      color: magenta;
+    }
+  </style>
+  <h1>Hey, what's up?</h1>
+`;
+
+class AppRoot extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.querySelector('h1').innerText = this.getAttribute('text');
+  }
+}
+
+window.customElements.define('app-root', AppRoot);
