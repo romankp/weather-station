e3a356f67ef315f72b13574da0dff950254fa11b
Roman <romankp@gmail.com>
2021-12-08 20:41:29 -0500

    add date variables

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -9,6 +9,13 @@ import {
 const baseUrl = process.env.BASE_URL;
 const stationId = process.env.STATION_ID;
 
+const currentTime = new Date();
+const tidalCutoff = new Date().setHours(18, 47, 30);
+const isAfterCutoff = currentTime >= tidalCutoff;
+const startDate = constructQueryDate(currentTime, false);
+const endDate = constructQueryDate(currentTime, isAfterCutoff);
+const urlFull = buildFullURL(baseUrl, stationId, startDate, endDate);
+
 class Root extends Component {
   constructor() {
     super();
