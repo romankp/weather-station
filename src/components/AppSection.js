699fab986cc29ff8dddf2e2abb3087e04e465008
Roman <romankp@gmail.com>
2022-02-21 17:25:12 -0500

    tides condition & ol

--- src/components/AppSection.js
+++ src/components/AppSection.js
@@ -22,7 +22,15 @@ export class AppSection extends HTMLElement {
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
 
-    this.shadowRoot.querySelector('h3').innerText =
-      subHeadings[this.getAttribute('type')];
+    this.type = this.getAttribute('type');
+
+    // Dynamic content
+    this.shadowRoot.querySelector('h3').innerText = subHeadings[this.type];
+
+    if (this.type === 'tides') {
+      const childOL = document.createElement('ol');
+
+      this.shadowRoot.appendChild(childOL);
+    }
   }
 }
