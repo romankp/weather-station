a68d2940bad420f0b06ffa835c5c0d5bc6888598
Roman <romankp@gmail.com>
2022-07-05 23:16:30 -0400

    initial imports for main

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
+import { buildForecastURL, fetchData } from '../src/utils/apiAccess.js';
+import {
+  getCurrentDateString,
+  constructQueryDate,
+  localizeTime,
+} from '../src/utils/dateUtils.js';
+// import { AppSection } from '../src/components/AppSection.js';
+
+// We don't destructure here because of a limitation in how Parcel interacts with .env variables
+// const gridString = process.env.FORECAST_GRID;
+
+// const forecastURL = buildForecastURL(gridString);
+const currentTime = new Date();
+
 function App() {
   return (
-    <div id="root">
+    <>
       <h1>Weather Station</h1>
-      <h2></h2>
+      <h2>{getCurrentDateString(currentTime)}</h2>
       <app-section type="current">
         <h4 slot="section-content">
           HEADER TEST <span>Wow! So span!</span>
@@ -10,7 +24,7 @@ function App() {
       </app-section>
       <app-section type="tides"></app-section>
       <app-section type="wind"></app-section>
-    </div>
+    </>
   );
 }
 
